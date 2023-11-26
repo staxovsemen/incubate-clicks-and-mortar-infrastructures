7b481343f0529763f9221fb252a5002e8167167a
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-11-26 05:02:21 -0500

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,40 @@
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
+
+    main()
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+import random
+def main():
+    for item in data:
+def generate_random_data():
+    return data        print(f"Random Number: {item}")
+
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+    return data
+import random
+def main():
+    for item in data:
+    data = generate_random_data()
+
+
+if __name__ == "__main__":
+def generate_random_data():
+        print(f"Random Number: {item}")
+import random
+    data = generate_random_data()
+def main():
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+
+    for item in data:
+
+    return data
